f6ffbab132170c913e659fa228111d5260e8bee0
Chen Rong <rong19036@gmail.com>
2021-08-17 16:17:34 +0800

    add excel

--- daily_account.py
+++ daily_account.py
@@ -1,13 +1,16 @@
 #讀取檔案
 
 #寫入檔案
-with open('daily_account.csv','w') as f :
-    product =[]
-    while True:
-        name = input('請輸入商品名稱:')
-        if name == 'q':
-            break
-        price = input('請輸入價格:')
-        product.append([name,price])
-    print(product)#輸出資料
+product =[]
+while True:
+    name = input('請輸入商品名稱:')
+    if name == 'q':
+        break
+    price = input('請輸入價格:')
+    product.append([name,price])
+    #print(product)#輸出資料
 
+with open('daily_account.csv','w') as f :
+    for p in product:
+        f.write(p[0] + ',' + p[1] + '\n')
+    
