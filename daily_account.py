3ec53dc23d48fdfe80c500ca4b2703480c99a56b
Chen Rong <rong19036@gmail.com>
2021-08-20 15:50:06 +0800

    refactor

--- daily_account.py
+++ daily_account.py
@@ -1,16 +1,26 @@
 #讀取檔案
+product = []
 
-#寫入檔案
-product =[]
-while True:
-    name = input('請輸入商品名稱:')
-    if name == 'q':
-        break
-    price = input('請輸入價格:')
-    product.append([name,price])
-    #print(product)#輸出資料
-
-with open('daily_account.csv','w') as f :
+def read_file(file):
+        with open(file,'r',encoding = 'utf-8')as f:
+            print(f)
+def user_input():
+    while True:
+        pro = input('請輸入商品:')
+        if pro == 'q':
+            break
+        price = input('請輸入價格:')
+        product.append([pro,price])
+def print_product():
+    print('新加入的商品:')
     for p in product:
-        f.write(p[0] + ',' + p[1] + '\n')
-    
+        print(p)
+def write_file(file):
+    with open(file,'w',encoding = 'utf-8') as f:
+        for p in product:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+read_file('daily_account.csv')
+user_input()
+print_product()
+write_file('daily_account.csv')
